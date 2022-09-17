7ef941398cfb7a26d748e6d32b7fc533a0d1e907
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-09-16 21:35:41 -0300

    Habilitando a exportação de Schemas do Room

--- app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
+++ app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
@@ -6,7 +6,7 @@ import br.com.alura.orgs.database.converter.Converters
 import br.com.alura.orgs.database.dao.ProdutoDao
 import br.com.alura.orgs.model.Produto
 
-@Database(entities = [Produto::class], version = 1)
+@Database(entities = [Produto::class], version = 1, exportSchema = true)
 @TypeConverters(Converters::class)
 abstract class AppDatabase: RoomDatabase() {
 
