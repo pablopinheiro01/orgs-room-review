f10300d6864860594edea0f4010ef5af971a5510
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-09-21 21:02:10 -0300

    Testes de cancelamento de coroutine utilizando JOBS + handlers + CouroutineName

--- app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
@@ -41,10 +41,29 @@ class ListaProdutosActivity : AppCompatActivity() {
             Log.i(TAG, "Erro do Coroutine $throwable")
             Toast.makeText(this, "ocorreu um erro", Toast.LENGTH_LONG).show()
         }
-        val scope = MainScope()
 
+        val scope = MainScope()
+        val job = Job() //job pode ser compartilhado entre coroutines
+
+        // codigo de teste executando um cancelamento com JOB
+        val jobPrimario = scope.launch(job + handler + Dispatchers.IO + CoroutineName("Primaria")) {
+            Log.i(TAG, "onResume: Contexto da coroutine ${coroutineContext}")
+            //caso saia da tela, minimize o app, a coroutine continuara executando
+            repeat(1000){
+                Log.i("onResume", "onResume: Coroutine está em execução $it")
+                delay(1000L)
+//                job.cancel()
+            }
+        }
+
+        // --------------------------------------------------------------------------
+        //codigo de teste lançando exception
         //o builder launch consegue receber por parametro o handler para tratamento de erro
-        scope.launch(handler){
+        scope.launch(job + handler){
+            //cancelando a coroutine anterior depois de 1 segundo da execução dessa coroutine
+            delay(1000L)
+//            job.cancel() //cancelando todas as coroutines q fazem referencia ao job
+//            jobPrimario.cancel() //cancela o retorno do builder da coroutine anterior (acima)
 //            throw Exception("erro qualquer dentro do launch")
             val produtos = withContext(Dispatchers.IO) {
                 produtoDao.buscaTodos()
